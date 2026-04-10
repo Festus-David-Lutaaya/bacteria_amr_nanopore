1045876d5642330bb0472e820c8b9870bd2defd8
Festus David Lutaaya <lutaayafestus@gmail.com>
2026-04-10 19:13:27 +0300

    updated panaroo

--- panaroo.sh
+++ panaroo.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --job-name=panaroo_pipeline
+#SBATCH --output=panaroo_%j.log
+#SBATCH --error=panaroo_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=24:00:00
+#SBATCH --mem=0
+
+set -euo pipefail
+
 
 panaroo -i gffs/*.gff3 \
         -o panaroo_results \
